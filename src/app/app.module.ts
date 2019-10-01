f71da2ee49a67f6928b74819687b5fd8b9a4eb8f
tounga <tounga.franck-extern@renault.com>
2019-10-01 21:07:15 +0200

    Integration de la page blog avec son routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {MDBBootstrapModule} from 'angular-bootstrap-md';
+import {Routes, RouterModule} from '@angular/router';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {BlogComponent} from './blog/blog.component';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+const appRoutes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'blog', component: BlogComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
