b7ce538871f57102a2862b60206ee895403ff50c
tounga <tounga.franck-extern@renault.com>
2019-11-01 13:24:44 +0100

    redirection vers les articles toungafranck.com

--- src/app/model/article.model.ts
+++ src/app/model/article.model.ts
@@ -1,14 +1,16 @@
 export class Article {
 
   public title: string;
-  public url: string;
+  public urlImg: string;
   public preview: string;
+  public location: string;
   public dateCreation: Date;
 
-  constructor(title: string, url: string, preview: string, dateCreation: Date) {
+  constructor(title: string, url: string, preview: string, location: string, dateCreation: Date) {
     this.title = title;
-    this.url = url;
+    this.urlImg = url;
     this.preview = preview;
+    this.location = location;
     this.dateCreation =  dateCreation;
   }
 }
