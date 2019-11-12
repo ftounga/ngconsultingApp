2babbe349d88b32dfa570ad0efbf5329a416884e
tounga <tounga.franck-extern@renault.com>
2019-11-12 17:37:18 +0100

    Configuration des datasources H2 et postgresql

--- api/src/main/java/com/ngconsulting/api/controller/LoginController.java
+++ api/src/main/java/com/ngconsulting/api/controller/LoginController.java
@@ -1,11 +1,18 @@
 package com.ngconsulting.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.sql.DataSource;
+import java.util.List;
+
 @RestController
 public class LoginController {
+
+    @Autowired
+    private List<DataSource> dataSources;
 
     @GetMapping("/login")
     @ResponseBody
