b4f520d01de09a3f2035c1de2194a28bda7c68fd
tounga <tounga.franck-extern@renault.com>
2019-12-28 15:51:00 +0100

    Optimisation de l'affichage des messages du toastr sur l interface graphique

--- app/src/app/AuthGuard.ts
+++ app/src/app/AuthGuard.ts
@@ -1,18 +1,18 @@
 import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
 import {OAuthService} from 'angular-oauth2-oidc';
+import {ToastrService} from 'ngx-toastr';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-  constructor(private oauthService: OAuthService, private router: Router) {}
+  constructor(private oauthService: OAuthService, private router: Router, private toastrService: ToastrService) {}
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot, ): boolean {
     if (this.oauthService.hasValidIdToken()) {
       return true;
     }
-
-    this.router.navigate(['/home'], {queryParams: {isLogged: false}});
+    this.toastrService.warning('Vous devez vous connecter', 'Authentification');
     return false;
   }
 }
--- app/src/app/home/home.component.ts
+++ app/src/app/home/home.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Article} from '../model/article.model';
 import {BlogService} from '../services/blog.service';
-import {ActivatedRoute} from '@angular/router';
-import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-home',
@@ -12,17 +10,12 @@ import {ToastrService} from 'ngx-toastr';
 export class HomeComponent implements OnInit {
 
   articles: Article[] ;
-  constructor(private blogService: BlogService, private route: ActivatedRoute, private toastrService: ToastrService) {}
+  constructor(private blogService: BlogService) {}
 
   ngOnInit() {
     this.blogService.getAllArticles().subscribe( response => {
       console.log(response);
       this.articles = response;
-    });
-    this.route.queryParams.subscribe(queryParam => {
-      if (queryParam.isLogged === 'false') {
-        this.toastrService.warning('Vous devez vous connecter', 'Authentification');
-      }
     });
   }
 
