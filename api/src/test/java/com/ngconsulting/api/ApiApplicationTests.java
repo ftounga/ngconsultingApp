5b57690a8aefdb43f5ac02529bdf68cc32c6c3c4
TOUNGA01 <franck.tounga@5thfloor.be>
2022-04-26 16:57:42 +0200

    remove test ApiApplicationTests.java

--- api/src/test/java/com/ngconsulting/api/ApiApplicationTests.java
+++ api/src/test/java/com/ngconsulting/api/ApiApplicationTests.java
@@ -4,11 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("test")
+//@SpringBootTest
+//@ActiveProfiles("test")
 class ApiApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
