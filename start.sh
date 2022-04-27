551fb7fa453d0e1cef4ca6ff5763d67117d908a8
TOUNGA Franck <ntounga@gmail.com>
2022-04-27 18:43:55 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
-MAIL_RECEIVER=$1
-if [ $# = 0 ]
-then
-    echo "No arguments recieved ! You should specify your email"
-	exit 1 
-fi
 echo "docker-compose down"
-echo "Every email will be sent to $MAIL_RECEIVER"
+echo "Every email will be sent to tounga.franck@ng-itconsulting.com"
 #docker-compose down --rmi all
 docker-compose down
 echo "docker-compose up"
 export MAIL_RECEIVER=$MAIL_RECEIVER
-docker-compose up
+docker-compose up tounga.franck@ng-itconsulting.com
 # tounga.franck@ng-itconsulting.com
 # force rebuild all image
 #docker-compose up --force-recreate --build
