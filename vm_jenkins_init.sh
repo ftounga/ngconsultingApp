3da64576888e72a2460074cbbe3ec2a1773a86cd
TOUNGA01 <franck.tounga@5thfloor.be>
2022-04-28 15:56:03 +0200

    fix jenkins key rpm

--- vm_jenkins_init.sh
+++ vm_jenkins_init.sh
@@ -2,7 +2,7 @@
 
 sudo wget –O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
 sudo mv jenkins.repo /etc/yum.repos.d/jenkins.repo
-sudo rpm ––import https://pkg.jenkins.io/redhat/jenkins.io.key
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo yum install jenkins
 sudo systemctl start jenkins
 sudo systemctl enable jenkins
