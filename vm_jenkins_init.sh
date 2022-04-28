dd59f52f0aa4e7462b6b7bd45fe3c7d9598bf2b4
TOUNGA01 <franck.tounga@5thfloor.be>
2022-04-28 15:44:03 +0200

    fix jenkins repo location

--- vm_jenkins_init.sh
+++ vm_jenkins_init.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 sudo wget –O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+sudo mv jenkins.repo /etc/yum.repos.d/jenkins.repo
 sudo rpm ––import https://pkg.jenkins.io/redhat/jenkins.io.key
 sudo yum install jenkins
 sudo systemctl start jenkins
